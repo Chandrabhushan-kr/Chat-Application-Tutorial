75c1cdfe639d899b1cb0e8197cf0428c64dfaa68
Chandrabhushan kumar <chbhusan38@gmail.com>
2024-09-24 13:36:24 +0530

    Login api route

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -10,11 +10,11 @@ export const signup = async (req, res, next) => {
   validUser = await User.findOne({ email });
 
   if (validUser) {
-    return next(errorHandler(400,"User already exists"))
+    return next(errorHandler(400, "User already exists"));
   }
 
   if (password != confirmPassword) {
-   return next(errorHandler(400, "Password don't match"));
+    return next(errorHandler(400, "Password don't match"));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -36,17 +36,41 @@ export const signup = async (req, res, next) => {
 
     await newUser.save();
 
-    res.cookie("access_token",token,{httpOnly: true}).status(201).json({
+    res.cookie("access_token", token, { httpOnly: true }).status(201).json({
       _id: newUser._id,
       username: newUser.username,
       email: newUser.email,
       profilePic: newUser.profilePic,
     });
   } catch (error) {
-    next(error)
+    next(error);
   }
 };
 
-export const login = (req, res) => {};
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "wrong credentials"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    res.cookie("access_token", token, { httpOnly: true }).status(200).json({
+      _id: validUser._id,
+      username: validUser.username,
+      email: validUser.email,
+      profilePic: validUser.profilePic,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const logout = (req, res) => {};
