fe43e24ec608d54886de98eb9779a396243a28c1
Chandrabhushan kumar <chbhusan38@gmail.com>
2024-09-24 09:07:54 +0530

    SignUp api route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -17,9 +18,17 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
   res.send("Hello worlds");
 });
+
+//import routes
+import authRoutes from "./routes/auth.routes.js";
+
+app.use("/api/auth",authRoutes)
 
 app.listen(PORT, () => {
   console.log("Server is running on port: " + PORT);
