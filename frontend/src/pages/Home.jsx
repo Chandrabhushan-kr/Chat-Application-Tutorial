e099cf2676b55e6485d679a313832f0376a8ab16
Chandrabhushan kumar <chbhusan38@gmail.com>
2024-09-25 11:21:04 +0530

    Homepage sidebar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Sidebar from "../component/sidebar/Sidebar";
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className="flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-200">
+      <Sidebar />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
