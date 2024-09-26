24aa6148f27566e3f633e89a2575790f8292d1d2
Chandrabhushan kumar <chbhusan38@gmail.com>
2024-09-26 12:11:18 +0530

    signup functionality

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+import { AuthContextProvider } from "./context/AuthContext.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
